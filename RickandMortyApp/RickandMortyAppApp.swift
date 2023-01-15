b724f79fbb97ced64851fa0883b64862c6927775
laumonfort <l.monfortgomez@gmail.com>
2023-01-15 01:27:41 +0100

    Initial Commit

---
+++ RickandMortyApp/RickandMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyAppApp.swift
+//  RickandMortyApp
+//
+//  Created by Laura Monfort Gomez on 15/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickandMortyAppUITests/RickandMortyAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RickandMortyAppUITestsLaunchTests.swift
+//  RickandMortyAppUITests
+//
+//  Created by Laura Monfort Gomez on 15/1/23.
+//
+
+import XCTest
+
+class RickandMortyAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
