a01efd02eb880327ffa47628dd267d5644f318be
laumonfort <l.monfortgomez@gmail.com>
2023-02-21 21:24:42 +0100

    Improve error handling providing more informative error messages. Refactor the RickMortyApi class to use an enum for result handling and add a custom enum for network errors

--- RickandMortyApp/Networking/APIClient.swift
+++ RickandMortyApp/Networking/APIClient.swift
@@ -3,32 +3,48 @@
 //  RickandMortyApp
 //
 //  Created by Laura Monfort Gomez on 16/1/23.
-
+//
+// MARK: Rick and Morty API, Alamofire Request: an HTTP networking library written in Swift.
 
 import Foundation
 import Alamofire
 
-// MARK: Rick and Morty API, Alamofire Request: an HTTP networking library written in Swift.
-final class RickMortyApi {
+enum Result<T> {
+    case success(T)
+    case failure(Error)
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidResponse
+    case decodingError
+    case serverError(Int)
+    case networkError(Error)
+}
+
+protocol RickMortyApiProtocol {
+    func loadCharacter(completion: @escaping (Result<[Results]>) -> Void)
+}
+
+final class RickMortyApi: RickMortyApiProtocol {
     private let baseUrl = "https://rickandmortyapi.com/api/character"
-    
-    func loadCharacter(completion: @escaping (Result<[Results], Error>) -> Void) {
-            AF.request(baseUrl).responseDecodable(of: Character.self) { response in
-                switch response.result {
-                case .success(let charactersList):
-                    completion(.success(charactersList.results))
-                case .failure(let error):
-                    if let data = response.data, let apiError = try? JSONDecoder().decode(APIError.self, from: data) {
-                        completion(.failure(apiError))
+
+    func loadCharacter(completion: @escaping (Result<[Results]>) -> Void) {
+        AF.request(baseUrl).responseDecodable(of: Character.self) { response in
+            switch response.result {
+            case .success(let character):
+                completion(.success(character.results))
+            case .failure(let error):
+                if let response = response.response {
+                    if 400..<500 ~= response.statusCode {
+                        completion(.failure(NetworkError.serverError(response.statusCode)))
                     } else {
-                        completion(.failure(error))
+                        completion(.failure(NetworkError.networkError(error)))
                     }
+                } else {
+                    completion(.failure(NetworkError.networkError(error)))
                 }
             }
         }
     }
-
-    struct APIError: Error, Decodable {
-        let error: String
-    }
-
+}
